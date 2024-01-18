c91b2262a0802e9b88734f0936b6ecce303acbcc
asyrawih <hasyrawi@gmail.com>
2024-01-18 18:40:24 +0700

    feat(config): attach custom config into git

---
+++ lua/custom/mappings.lua
@@ -0,0 +1,48 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+
+    --  format with conform
+    ["<leader>fm"] = {
+      function()
+        require("conform").format()
+      end,
+      "formatting",
+    },
+  },
+  v = {
+    [">"] = { ">gv", "indent" },
+  },
+}
+
+-- overides
+M.tabufline = {
+  n = {
+    -- modify close buffer
+    ["<s-w>"] = {
+      function()
+        require("nvchad.tabufline").close_buffer()
+      end,
+      "Close buffer",
+    },
+  },
+}
+
+M.telescope = {
+  n = {
+    ["<leader>tt"] = { "<cmd> Telescope live_grep <CR>", "telescope live grep" },
+  },
+}
+
+M.lspconfig = {
+  n = {
+    ["<leader>o"] = { "<cmd> Lspsaga outline<CR>", "lsp saga outline" },
+  }
+}
+
+
+
+return M
